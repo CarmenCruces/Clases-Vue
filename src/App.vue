7434eac52e032cad6b9041b99af94c3e1a32cd39
CarmenCruces <karmelacfer@gmail.com>
2023-02-22 20:10:00 +0100

    changed

--- src/App.vue
+++ src/App.vue
@@ -5,21 +5,15 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
+      
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <HelloWorld msg="Marketplace" />
+    
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/myview">My View</RouterLink>
+        <RouterLink to="/secondview">SecondView</RouterLink>
       </nav>
     </div>
   </header>
